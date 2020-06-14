f00d8de9a54bde5db67eafa6785bc73bb7b6e0cb
IvanDumenko <62713805+IvanDumenko@users.noreply.github.com>
2020-06-14 23:35:47 +0300

    basic-committ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+export interface navList {
+  name: string,
+  id: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weather-widget';
+  title = 'my-profile';
+  navlist: navList[] = [
+    {name: "About",id: 0},
+    {name: "Experience",id: 1},
+    {name: "Education",id: 2},
+    {name: "Skills",id: 3},
+    {name: "Contact",id: 4}
+  ]
+  index: number = 0;
+  tg(id: number) {
+    this.index = id;
+    console.log(id);
+  }
 }
